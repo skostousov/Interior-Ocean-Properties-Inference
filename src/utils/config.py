b1ab6060a7d51345abaaa9f13af222429e8523e9
skostousov <93064193+skostousov@users.noreply.github.com>
2025-05-14 15:01:35 -0400

    added saved models directory

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,3 +7,4 @@ with open(_REPO_ROOT/"configs"/"config.yaml", "r") as f:
     RAW_CONFIG = yaml.safe_load(f)
 
 DATA_DIR = (_REPO_ROOT / RAW_CONFIG["data_dir_relative_to_project_root"]).resolve()
+SAVED_MODELS_DIR = (_REPO_ROOT / RAW_CONFIG["model_save_dest"]).resolve()
