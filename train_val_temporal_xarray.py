1c88e8a7156b4344ddec9de4c4d7038944697c30
skostousov <samuel.kostousov@gmail.com>
2025-06-06 11:00:27 -0400

    added immediate results

--- train_val_temporal_xarray.py
+++ train_val_temporal_xarray.py
@@ -17,6 +17,8 @@ from torch.optim import AdamW
 import time
 from utils.splitter import train_val_test_split_temp
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+import matplotlib.pyplot as plt
+
 
 import sys, importlib
 sys.modules.setdefault("numpy._core", importlib.import_module("numpy.core"))
@@ -177,8 +179,8 @@ if cfg["training"]["immediate_test"]:
     batch_size=1,
     shuffle=False,
     num_workers=0,
-    pin_memory=True,
-)
+    pin_memory=True,)
+
     model.eval()
     loss = 0
 
@@ -212,7 +214,99 @@ if cfg["training"]["immediate_test"]:
         if len(after_model) > 0:
             with open(filepath, "ab") as f:
                 pickle.dump(after_model, f)
-print(f"Total loss: {loss / len(test_dataloader)}")
-with open(info_path, 'a') as f:
-    f.write(f"total_test_loss: {loss / len(test_dataloader)}\n")
+    print(f"Total loss: {loss / len(test_dataloader)}")
+    with open(info_path, 'a') as f:
+        f.write(f"total_test_loss: {loss / len(test_dataloader)}\n")
 
+    test_data = TestSubsetRegression(data, test_idx)
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test_data,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+    )
+
+    def iter_pickled_batches(file_path):
+        """Generator to yield batches from a pickled file."""
+        with open(file_path, "rb") as f:
+            while True:
+                try:
+                    yield pickle.load(f)
+                except EOFError:
+                    break
+                except pickle.UnpicklingError:
+                    print("Unpickling error encountered.")
+                    break
+
+    def iter_pickled_batches(file_path):
+        """Generator to yield batches from a pickled file."""
+        with open(file_path, "rb") as f:
+            while True:
+                try:
+                    yield pickle.load(f)
+                except EOFError:
+                    break
+                except pickle.UnpicklingError:
+                    print("Unpickling error encountered.")
+                    break
+
+    time_steps = get_t_from_pickled(filepath)
+
+    mean, std = data.mean_label, data.std_label
+    lat_range, lon_range = data.feature_map.shape[-2], data.feature_map.shape[-1]
+
+    pred_maps = []
+    label_maps = []
+    for t in time_steps:
+        pred_maps.append(np.zeros((lat_range, lon_range)))
+        label_maps.append(np.zeros((lat_range, lon_range)))
+
+    for batch in iter_pickled_batches(filepath):
+        for entry in batch:
+            entry["label_unnorm"] = ((entry["label"] * std) + mean).item()
+            entry["pred_unnorm"] = ((entry["pred"] * std) + mean).item()
+            t = entry["month"].item()
+            t_idx = time_steps.index(t)
+            lat, lon = entry["centre"][0].item(), entry["centre"][1].item()
+            print(f"Processing time step: {t}, lat: {lat}, lon: {lon}")
+            pred_maps[t_idx][lat, lon] = entry["pred_unnorm"]
+            label_maps[t_idx][lat, lon] = entry["label_unnorm"]
+
+    mae_loss = nn.L1Loss()
+
+    fig, ax = plt.subplots(len(time_steps), 2, figsize=(15, 8* len(time_steps)))
+    for i, t in enumerate(time_steps):
+        pred_map = pred_maps[i]
+        label_map = label_maps[i]    
+        im_0 = ax[i, 0].imshow(label_map, origin='lower', vmin=0, vmax=100, cmap='viridis')
+        ax[i, 0].set_title('Target Map for Time Step ' + str(t))
+        ax[i, 0].set_xlabel('Longitude Index')
+        ax[i, 0].set_ylabel('Latitude Index')
+        fig.colorbar(im_0, label='Actual MLD (m)', ax=ax[i, 0])
+        im_1 = ax[i, 1].imshow(pred_map, origin='lower', vmin=0, vmax=100, cmap='viridis')
+        mae = mae_loss(torch.tensor(label_map), torch.tensor(pred_map)).item()
+        ax[i, 1].set_title('Prediction Map for Time Step ' + str(t) + " MAE: " + f"{mae:.7f}")
+        ax[i, 1].set_xlabel('Longitude Index')
+        ax[i, 1].set_ylabel('Latitude Index')
+        fig.colorbar(im_1, label="Predicted MLD (m)", ax=ax[i, 1])
+    plt.suptitle(f"{model_name} \n Results")
+    plt.subplots_adjust(hspace=0.5)
+    fig.savefig(model_path / "resultsxarray.png", dpi=600)
+
+    fig, ax = plt.subplots(len(time_steps), 1, figsize=(6, 5* len(time_steps)))
+    for i, t in enumerate(time_steps):
+        if len(time_steps) == 1:
+            ax = [ax]  # Ensure ax is iterable if there's only one time step
+        pred_map = pred_maps[i]
+        label_map = label_maps[i]    
+        im_0 = ax[i,].imshow(abs(label_map - pred_map), origin='lower', vmin=0, vmax=80, cmap='viridis')
+        ax[i].set_title('Difference Map for Time Step ' + str(t))
+        ax[i].set_xlabel('Longitude Index')
+        ax[i].set_ylabel('Latitude Index')
+        fig.colorbar(im_0, label='MLD MAE (m)', ax=ax[i])
+    plt.suptitle(f"{model_name} \n Difference Results")
+    plt.subplots_adjust(hspace=0.3)
+    plt.show()
+    fig.savefig(model_path / "results_diffxarray.png", dpi=600)
