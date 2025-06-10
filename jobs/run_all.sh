8aa10c61f07aa1c72ecdaf67c0a1732d520d16b0
skostousov <samuel.kostousov@gmail.com>
2025-06-10 14:42:17 -0400

    updated run

--- jobs/run_all.sh
+++ jobs/run_all.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 cd ~/$SCRATCH/OceanPropInfSatImgScratch/OceanPropInfSatImg
 
-prevJob_id=$(sbatch jobs/train.slurm UNetRegressionSE --num_epochs "5" --data_processors netcdf4 | awk '{print $4}')
-MODEL=
-for i in {1..10}
-do
-    prevJob_id=$(sbatch --dependency=afterok:$prevJob_id jobs/continue_train.slurm $MODEL 5 | awk '{print $4}')
+prevJob_id=$(sbatch jobs/train.slurm UNetRegressionSE --num_epochs "1" --data_processors netcdf4 | awk '{print $4}')
+
+while squeue -h -j "$prevJob_id" | grep -q .; do
+  sleep 10
 done
-echo "model_trained"
+
+MODEL=$(tail -n 1 logs/latest_model_dir.txt)
+
+echo "initial training loop has run, model: $MODEL"
+
+for i in {1..2}
+do
+    prevJob_id=$(sbatch --dependency=afterok:$prevJob_id jobs/continue_train.slurm $MODEL 1 | awk '{print $4}')
+done
+
+echo "model trained"
 sbatch --dependency=afterok:$prevJob_id jobs/continue_train.slurm $MODEL
 echo "inference has been run"
