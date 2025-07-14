9e294b6bff5efa5d14276fdda60083c56ec1258d
skostousov <samuel.kostousov@gmail.com>
2025-07-14 09:54:26 -0400

    asfAFW

--- hptuning_UNETSE_ray_lower_res.py
+++ hptuning_UNETSE_ray_lower_res.py
@@ -16,7 +16,7 @@ from data.argo.alternate_dataset import myDataset
 
 print(os.getcwd())
 
-season = "spring"
+season = "summer"
 
 temp_dir = tempfile.mkdtemp(prefix=f"unetse_coarsen_{season}")
 ray.init(ignore_reinit_error=True, _temp_dir=temp_dir)
